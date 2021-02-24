311b9cceec43a9738a229d68254342bcf802cb0d
Jay Fernandez <fernandezj4@spu.edu>
2021-02-24 12:03:29 -0800

    Initial Version

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 
+int fib ( int n) {
+    if (n == 0) return 0;
+    if (n==1) return 1;
+    return fib(n-1) + fib (n-2);
+}
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    int m;
+    int n;
+    n = 10;
+    m = fib ( 5);
+    std ::cout << "fib(" << n << ")=" << m << std::endl;
+    std::cout << "Bye for now." << std::endl;
     return 0;
 }
